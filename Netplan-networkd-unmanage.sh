772ba08930d7536cad03cb89504b8f710cbfda9d
jjjttwkr <jjjttwkr@outlook.com>
2026-07-02 18:29:45 +0800

    Fix typo in package name in nosnap.pref and add confirmation prompts in Netplan script

--- Netplan-networkd-unmanage.sh
+++ Netplan-networkd-unmanage.sh
@@ -2,4 +2,7 @@
 #
 #For systemd-networkd use ONLY.
 #Do NOT use on a NetworkManager system.
+echo For systemd-networkd use ONLY (on Ubuntu Server Default Installation)
+echo Press Enter to continue
+read
 sudo systemctl disable --now netplan-configure
